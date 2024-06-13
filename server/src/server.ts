5fe64197b0206eabbd872f2bc96cb03c2c3a870b
liusong <404570978@qq.com>
2024-06-13 09:26:41 +0800

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,30 +15,6 @@ import { RSSService } from './services/rss';
 const host = `https://telegra.ph`;
 
 export const app = (db: DB, env: Env) => new Elysia({ aot: false })
-    .get('/test', async () => {
-        // return fetch(`https://telegra.ph/file/44f30de6accd493b205c8.png`);
-        // Fetch the resource
-        const response = await fetch(`https://telegra.ph/file/44f30de6accd493b205c8.png`);
-
-        // Check if the fetch was successful
-        if (!response.ok) {
-            throw new Error(`Failed to fetch image: ${response.status} ${response.statusText}`);
-        }
-
-        // Create a new Response object to ensure headers can be manipulated
-        const newResponse = new Response(await response.blob(), {
-            status: response.status,
-            statusText: response.statusText,
-            headers: {
-                // You may need to explicitly set some headers important for the client here,
-                // especially if they are needed for correct content handling (e.g., Content-Type).
-                // Example:
-                // 'Content-Type': response.headers.get('Content-Type'),
-            },
-        });
-
-        return newResponse;
-    })
     .use(cors({
         aot: false,
         origin: '*',
@@ -70,6 +46,29 @@ export const app = (db: DB, env: Env) => new Elysia({ aot: false })
     //         fileName: t.String()
     //     })
     // })
+    .get('/file/:fileName', async ({ params: { fileName }}) => {
+        // return fetch(`https://telegra.ph/file/44f30de6accd493b205c8.png`);
+        // Fetch the resource
+        const response = await fetch(`${host}/file/${fileName}`);
+
+        // Check if the fetch was successful
+        if (!response.ok) {
+            throw new Error(`Failed to fetch image: ${response.status} ${response.statusText}`);
+        }
+        // Create a new Response object to ensure headers can be manipulated
+        const newResponse = new Response(await response.blob(), {
+            status: response.status,
+            statusText: response.statusText,
+            headers: {
+                // You may need to explicitly set some headers important for the client here,
+                // especially if they are needed for correct content handling (e.g., Content-Type).
+                // Example:
+                // 'Content-Type': response.headers.get('Content-Type'),
+            },
+        });
+
+        return newResponse;
+    })
     .onError(({ path, params, code }) => {
         if (code === 'NOT_FOUND')
             return `${path} ${JSON.stringify(params)} not found`
